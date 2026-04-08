8a7982bbdbd7c0e37699fad7539e950ff4a542d5
Schnelleq <kulikow.alexey07@gmail.com>
2026-04-08 21:28:22 +0300

    fix DataStruct.hpp

--- kulikov.aleksey/T2/DataStruct.hpp
+++ kulikov.aleksey/T2/DataStruct.hpp
@@ -12,9 +12,6 @@ struct DataStruct
     std::string key3;
 };
 
-unsigned long long parseBinary(const std::string& s);
-std::string toBinaryString(unsigned long long n);
-bool tryParseRecord(const std::string& record, DataStruct& ds);
 bool comparator(const DataStruct& a, const DataStruct& b);
 
 std::istream& operator>>(std::istream& in, DataStruct& ds);
